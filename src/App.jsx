4c4e750cd265b2af11deb11a31099c7b5254dc6d
unknown <nilzak.indigo05@gmail.com>
2024-07-17 17:01:26 +0100

    added message

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,21 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <h1>By Zaaboubi Hani</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+      <h2>My Dearest Sabrina</h2>
+      <p>From the moment we first met, my life has been filled with a joy I never knew was possible. Though miles and borders separate us, my love for you transcends distance and time. Each day that passes, my heart aches to be with you, to hold you close and share in the beauty of our love.</p>
+      <p>Montenegro’s landscapes may be breathtaking with its rugged mountains and serene coastlines, but nothing compares to the beauty that you bring into my life. I picture you there, amidst the stunning scenery, and I am filled with a sense of peace knowing that you are surrounded by such splendor, even though I wish I could be by your side.</p>
+     <p>I find myself dreaming of the day we will no longer have to count the days until we see each other again. I imagine walking hand in hand along the shores of Kotor Bay, exploring the old streets of Budva, and simply being together in the tranquility of our shared moments.</p>
+ <p>Your strength, your kindness, and your radiant smile inspire me every day. You have a way of making the ordinary feel extraordinary, and your love is the greatest gift I have ever received. I cherish the moments we have shared, and I eagerly look forward to the countless memories we will create together.</p>
+ <p>Until we are reunited, know that you are always in my thoughts and prayers. I send you all my love with every sunrise and wish you the warmth and comfort of our shared dreams.</p>
+ <p>Thank you for being my rock, my confidante, and my heart. I promise to treasure you forever, and I will always hold onto the hope of our next embrace.</p>
+ <h3>With all my love,</h3>
+ <h3>Your husband</h3>
+ <h2>Zaaboubi Hani</h2>
     </>
   )
 }
